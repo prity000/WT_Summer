26f6ede73c3ca1237201fa8cff4affa38ec9ae9c
prity000 <mdskylar@gmail.com>
2025-07-20 10:51:55 +0600

    Add files via upload

---
+++ Mid/Lab_Task_1/Mid/Lab_Task_1/lab 1.php
@@ -0,0 +1,58 @@
+<DOCTYPE html>
+<html>
+
+<title></title>
+<body>
+    <center>
+        <h1 style="color:blue;">AIUB</h1>
+        <h2 style="color:blue;">Registration Form</h2>
+    </center>
+    <table>
+    
+             <b>Complete the Registration</b><br>
+            <td>Full Name: </td><td> <input type= "text"> </td></tr>
+        <tr>
+               <td>Email:  </td><td> <input type ="text"></td></tr>
+               <tr>
+              <td> Password:  </td><td> <input type="" "text"></td></tr>
+        
+              <tr>
+                <td>Gender : </td><td><input type="radio" name="des">Male
+                                      <input type="radio" name="des">Other
+                                        </tr>
+                                        <tr>
+                                            <td> Language Known:</td><td> <input type="checkbox"> English
+                                                                           <input type="checkbox">Bangla
+                                                                           <input type="checkbox">Hindi
+</td>
+ </tr>
+   <tr>
+     <td>Country:</td>
+     <td><select name="Country:">
+        <option value="1">--select--</option>
+        <option value="2">--select--</option>
+        <option value="3">--select--</option>
+</select></td>
+</tr>
+
+     <tr>
+        <td>Date of Birth</td>
+        <td><input type="date"></td>
+</tr>
+<tr>
+    <td>Uplode photo: </td>
+    <td><input type="file"></td>
+    </tr>
+    <tr>
+    <td> Comments</td>
+    <td><input type="textbox"></td>
+</tr>
+<tr>
+   <td> <input type="button"name="Register"></td>
+   </tr>
+
+
+
+</table>
+</body>
+</html>
